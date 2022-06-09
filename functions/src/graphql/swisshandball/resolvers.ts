d8845f5d37101322df1821e2ed5a650a63bbaef7
Sandro Scalco <sandro.scalco@liitu.ch>
2022-06-09 18:59:30 +0200

    feat deps

--- functions/src/graphql/swisshandball/resolvers.ts
+++ functions/src/graphql/swisshandball/resolvers.ts
@@ -10,9 +10,8 @@ const fetch = require("node-fetch");
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const {convert} = require("html-to-text");
 
-// import * as functions from "firebase-functions";
-// const headers: any = {"Authorization": "Basic " + functions.config().swisshandball.token};
-const headers: any = {"Authorization": "Basic MTQwNTYxOk5uMFNRc3Fz"};
+import * as functions from "firebase-functions";
+const headers: any = {"Authorization": "Basic " + functions.config().swisshandball.token};
 
 export default {
 
