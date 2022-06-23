d725a6b2c2f9cee207cb981598510b3d224bc2e6
Sandro Scalco <sandro.scalco@liitu.ch>
2022-06-23 23:59:55 +0200

    fea lgos

--- functions/src/scheduler/utils/update.swissunihockey.ts
+++ functions/src/scheduler/utils/update.swissunihockey.ts
@@ -201,9 +201,13 @@ export async function updateClubsSwissunihockey(): Promise<any> {
 
 function getNextGame(index: number, gamesList: []): any {
   const nextGame: any = gamesList[index];
+  console.log(index);
+  console.log(gamesList);
+
   if (nextGame) {
     console.log(`Get Next Game with id ${nextGame.id} and date: ${nextGame.date} ${nextGame.time}`);
   }
+
   if (nextGame && nextGame.date.charAt(2) === ".") {
     return nextGame;
   } else {
