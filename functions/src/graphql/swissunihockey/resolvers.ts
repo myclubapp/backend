b4aff08a4812227b5e8bdc433133a2b1d84c0e66
Sandro Scalco <sandro.scalco@liitu.ch>
2023-03-08 13:13:13 +0100

    feat dom

--- functions/src/graphql/swissunihockey/resolvers.ts
+++ functions/src/graphql/swissunihockey/resolvers.ts
@@ -11,7 +11,7 @@ const fetch = require("node-fetch");
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const {convert} = require("html-to-text");
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const jsdom = require("jsdom");
+// const jsdom = require("jsdom");
 
 export default {
 
