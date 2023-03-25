da4e579c02a3f217a2a0273cd65658ef5d7a3df9
Sandro Scalco <sandro.scalco@liitu.ch>
2023-03-25 18:42:25 +0100

    feat game date value

--- functions/src/scheduler/utils/update.swissunihockey.ts
+++ functions/src/scheduler/utils/update.swissunihockey.ts
@@ -61,6 +61,8 @@ export async function updateGamesSwissunihockey(): Promise<any> {
         // Alles normal
         // game.date = "11.03.2023"
       }
+      console.log("Game ID: " + game.id);
+      console.log("Game Date: " + game.date);
       const gameDateTime: firebase.firestore.Timestamp = firebase.firestore.Timestamp.fromDate(new Date(`${game.date.substr(6, 4)}-${game.date.substr(3, 2)}-${game.date.substr(0, 2)}T${game.time}`));
 
       await db.collection("club").doc(`su-${club.id}`).collection("games").doc(`su-${game.id}`).set({
