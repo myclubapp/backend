6c989ace18e3f482c24a4139ea5b803e094872dc
Sandro <sandro.scalco@gmail.com>
2023-10-05 21:56:42 +0200

    feat update logs for user

--- functions/src/firestore/deleteMember.ts
+++ functions/src/firestore/deleteMember.ts
@@ -35,6 +35,7 @@ export async function deleteClubMember(snapshot: QueryDocumentSnapshot, context:
   console.log("deleteClubMember > Club Page via Administrator");
   const userId = context.params.userId;
   const clubId = context.params.clubId;
+  console.log("Auth User > " + context.auth?.uid);
 
   const adminUser = await auth.getUser(context.auth?.uid);
   if (adminUser && adminUser.customClaims && adminUser.customClaims[clubId]) {
