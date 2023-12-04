e263d4610734b757cfea16cd81f726be58efff95
Sandro <sandro.scalco@gmail.com>
2023-12-04 19:03:36 +0100

    feat update

---
+++ functions/src/scheduler/youtube.scheduler.ts
@@ -0,0 +1,51 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable require-jsdoc */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable max-len */
+import {EventContext} from "firebase-functions";
+import firebaseDAO from "../firebaseSingleton";
+const dbUA = firebaseDAO.instance.dbUA;
+const db = firebaseDAO.instance.db;
+const {google} = require("googleapis");
+
+// Initialize the YouTube API client
+const youtube = google.youtube({
+  version: "v3",
+  auth: "YOUR_API_KEY", // Replace with your API key
+});
+
+export async function youtubeScheduler(context: EventContext) {
+  try {
+    const response = await youtube.playlists.list({
+      part: "snippet",
+      id: "PL4GcRGPV7hzzrTUCK_ua0A4rvMc7WmhH9",
+    });
+
+    const playlist = response.data.items;
+    if (playlist.length === 0) {
+      throw new Error("Playlist not found");
+    }
+
+    for (const item of playlist) {
+      console.log(item);
+    }
+    return true;
+
+    // return playlist[0].snippet.channelId;
+    /* console.log("test");
+
+    const clubListRef = await db.collection("clubs").where("active", "==", true).where("type", "==", "swissunihockey").get();
+    for (const club of clubListRef.docs) {
+      console.log(club.data().externalId);
+      const uaClub = await dbUA.collection("clubs").where("suhvClubId", "==", club.data().externalId).get();
+      if (uaClub.exists) {
+        console.log("found old club with ID: " + uaClub.id);
+      }
+    } */
+  } catch (err) {
+    console.error(err);
+  }
+}
