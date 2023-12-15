da9616652ebc51c4ab484e91c2179f088a2ac4c5
Sandro Scalco <sandro.scalco@liitu.ch>
2023-12-15 19:32:03 +0100

    feat update helferevent confirm

--- functions/src/firestore/request/createClubRequest.ts
+++ functions/src/firestore/request/createClubRequest.ts
@@ -6,12 +6,14 @@
 /* eslint-disable max-len */
 import * as functions from "firebase-functions";
 import firebaseDAO from "../../firebaseSingleton";
-import webpush = require("web-push");
+// import webpush = require("web-push");
 import {QueryDocumentSnapshot} from "firebase-functions/lib/providers/firestore";
+import {Messaging} from "firebase-admin/lib/messaging/messaging";
 
 const db = firebaseDAO.instance.db;
+const messaging: Messaging = firebaseDAO.instance.messaging;
 
-const gcmAPIKey = functions.config().webpush.gcmapikey;
+/* const gcmAPIKey = functions.config().webpush.gcmapikey;
 const publicKey = functions.config().webpush.publickey;
 const privateKey = functions.config().webpush.privatekey;
 
@@ -20,7 +22,7 @@ webpush.setVapidDetails(
     "mailto:info@my-club.app",
     publicKey,
     privateKey
-);
+);*/
 
 export async function createClubRequest(snapshot: QueryDocumentSnapshot, context: functions.EventContext) {
   console.log("createClubRequest");
@@ -34,23 +36,89 @@ export async function createClubRequest(snapshot: QueryDocumentSnapshot, context
     "userProfileRef": userProfileRef.ref,
   });
 
-  // SEND REQUEST CONFIRMATION E-MAIL TO USER
-  await db.collection("mail").add({
-    to: userProfileRef.data()?.email,
-    template: {
-      name: "ClubRequestEmail",
-      data: {
-        clubName: clubRef.data().name,
-        firstName: userProfileRef.data()?.firstName,
-        lastName: userProfileRef.data()?.lastName,
+  if (userProfileRef.exists && userProfileRef.data().settingsPush) {
+    const userProfilePushRef = await db.collection("userProfile").doc(userProfileRef.id).collection("push").get();
+    for (const push of userProfilePushRef.docs) {
+      console.log(">> PUSH DEVICE: ", push.data());
+      if (push.data().platform === "web") {
+        // Send WebPush
+        /* const {statusCode, headers, body} = await webpush.sendNotification(JSON.parse(push.data().pushObject),
+            JSON.stringify( {
+              title: "Neue Beitrittsanfrage für deinen Verein: " + clubRef.data().name,
+              message: `${userProfileRef.data()?.firstName} ${userProfileRef.data()?.lastName} (${userProfileRef.data()?.email}) möchte deinem Verein beitreten.`,
+            }));
+        console.log(">> SEND PUSH EVENT: ", statusCode, headers, body); */
+      } else {
+        // Send native Push
+        const nativePush = await messaging.sendToDevice(push.data().token,
+            {
+              notification: {
+                title: "Neue Beitrittsanfrage für deinen Verein: " + clubRef.data().name,
+                message: `${userProfileRef.data()?.firstName} ${userProfileRef.data()?.lastName} (${userProfileRef.data()?.email}) möchte deinem Verein beitreten.`,
+                sound: "default",
+                badge: "0",
+              },
+            },
+        );
+        console.log(">> SEND Native PUSH EVENT: ", nativePush);
+      }
+    }
+  }
+  if (userProfileRef.exists && userProfileRef.data().settingsEmail === true) {
+    // SEND REQUEST CONFIRMATION E-MAIL TO USER
+    await db.collection("mail").add({
+      to: userProfileRef.data()?.email,
+      template: {
+        name: "ClubRequestEmail",
+        data: {
+          clubName: clubRef.data().name,
+          firstName: userProfileRef.data()?.firstName,
+          lastName: userProfileRef.data()?.lastName,
+        },
       },
-    },
-  });
+    });
+  }
 
   // SEND REQUEST E-MAIL TO CLUB ADMIN
   const receipient = [];
   console.log(`Get Admin from Club: ${clubId}`);
   const clubAdminRef = await db.collection("club").doc(clubId).collection("admins").get();
+
+  for (const admin of clubAdminRef.docs) {
+    const adminRef = await db.collection("userProfile").doc(admin.id).get();
+    if (adminRef.exists && adminRef.data().settingsPush) {
+      const userProfilePushRef = await db.collection("userProfile").doc(admin.id).collection("push").get();
+      for (const push of userProfilePushRef.docs) {
+        console.log(">> PUSH DEVICE: ", push.data());
+        if (push.data().platform === "web") {
+          // Send WebPush
+          /* const {statusCode, headers, body} = await webpush.sendNotification(JSON.parse(push.data().pushObject),
+              JSON.stringify( {
+                title: "Neue Beitrittsanfrage für deinen Verein: " + clubRef.data().name,
+                message: `${userProfileRef.data()?.firstName} ${userProfileRef.data()?.lastName} (${userProfileRef.data()?.email}) möchte deinem Verein beitreten.`,
+              }));
+          console.log(">> SEND PUSH EVENT: ", statusCode, headers, body); */
+        } else {
+          // Send native Push
+          const nativePush = await messaging.sendToDevice(push.data().token,
+              {
+                notification: {
+                  title: "Neue Beitrittsanfrage für deinen Verein: " + clubRef.data().name,
+                  message: `${userProfileRef.data()?.firstName} ${userProfileRef.data()?.lastName} (${userProfileRef.data()?.email}) möchte deinem Verein beitreten.`,
+                  sound: "default",
+                  badge: "0",
+                },
+              },
+          );
+          console.log(">> SEND Native PUSH EVENT: ", nativePush);
+        }
+      }
+    }
+    if (adminRef.exists && adminRef.data().settingsEmail === true) {
+      receipient.push(adminRef.data().email);
+    }
+  }
+  /*
   for (const admin of clubAdminRef.docs) {
     console.log(`Read Admin user for Club with id ${admin.id}`);
     const userProfileAdminRef = await db.collection("userProfile").doc(admin.id).get();
@@ -70,7 +138,7 @@ export async function createClubRequest(snapshot: QueryDocumentSnapshot, context
         }
       }
     }
-  }
+  }*/
 
   return db.collection("mail").add({
     to: receipient,
