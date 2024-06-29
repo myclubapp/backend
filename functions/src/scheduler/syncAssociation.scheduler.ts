af4674bf1f9d26554994ff2c7063d2cb14435a19
sansan88 <sandro.scalco@liitu.ch>
2024-06-29 13:12:39 +0200

    news

--- functions/src/scheduler/syncAssociation.scheduler.ts
+++ functions/src/scheduler/syncAssociation.scheduler.ts
@@ -164,7 +164,12 @@ async function updateClubNewsFromWordpress(): Promise<any> {
 
         const wpFeaturedMediaData = await fetch(news["_links"]["wp:featuredmedia"][0].href);
         const wpFeaturedMedia = await wpFeaturedMediaData.json();
-        const featuredMedia = wpFeaturedMedia.media_details.sizes.medium.source_url || wpFeaturedMedia.guid.rendered || wpFeaturedMedia.source_url;
+        let featuredMedia = wpFeaturedMedia.source_url || wpFeaturedMedia.guid.rendered;
+        try {
+          featuredMedia = wpFeaturedMedia.media_details.sizes.medium.source_url;
+        } catch (e) {
+          // console.log(e);
+        }
 
         await db.collection("club").doc(`${club.id}`).collection("news").doc(`${club.id}-${news.id}`).set({
           externalId: `${news["id"]}`,
