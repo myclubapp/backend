44be68dcd4108e2b8e4f06b363314fcac31df1cf
sansan88 <sandro.scalco@liitu.ch>
2024-12-16 21:55:57 +0100

    fix training create

--- functions/src/firestore/training/createTeamTraining.ts
+++ functions/src/firestore/training/createTeamTraining.ts
@@ -23,7 +23,7 @@ export async function createTeamTraining(snapshot: QueryDocumentSnapshot, contex
   const teamRef = await db.collection("teams").doc(trainingData.teamId).get();
   console.log("teamId" + trainingData.teamId);
 
-  const calculatedDate: Date = new Date();
+  // const calculatedDate: Date = new Date();
   let offSet = 0; // in milliseconds
   switch (trainingData.repeatFrequency) {
     case "D":
@@ -47,8 +47,14 @@ export async function createTeamTraining(snapshot: QueryDocumentSnapshot, contex
   console.log(`End Date used: ${trainingData.endDate}`);
 
   // Set Date based on first Training and Start Hours/minutes
-  calculatedDate.setTime(new Date(trainingData.startDate).getTime());
+  /* calculatedDate.setTime(new Date(trainingData.startDate).getTime());
+  calculatedDate.setHours(new Date(trainingData.timeFrom).getHours());
+  calculatedDate.setMinutes(new Date(trainingData.timeFrom).getMinutes());
+  calculatedDate.setSeconds(0);
+  calculatedDate.setMilliseconds(0); */
 
+  // Initialisierung des ersten Datums
+  const calculatedDate = new Date(new Date(trainingData.startDate).getTime());
   calculatedDate.setHours(new Date(trainingData.timeFrom).getHours());
   calculatedDate.setMinutes(new Date(trainingData.timeFrom).getMinutes());
   calculatedDate.setSeconds(0);
@@ -60,7 +66,6 @@ export async function createTeamTraining(snapshot: QueryDocumentSnapshot, contex
   calculatedEndDate.setMinutes(new Date(trainingData.timeTo).getMinutes());
   calculatedEndDate.setSeconds(0);
   calculatedEndDate.setMilliseconds(0);
-
 
   // Add Training Entry
   const newTrainingRef = await db.collection("teams").doc(trainingData.teamId).collection("trainings").add({
@@ -76,34 +81,42 @@ export async function createTeamTraining(snapshot: QueryDocumentSnapshot, contex
   console.log(`Calculated Start Date used: ${calculatedDate}`);
   console.log(`Calculated End Date used: ${calculatedEndDate}`);
 
+  // Schleife für alle Trainings
   do {
-    calculatedDate.setTime(calculatedDate.getTime() + offSet);
-
-    // Set EndDate
-    const calculatedEndDate = calculatedDate;
+    // Erstelle eine neue Kopie für das Enddatum
+    const calculatedEndDate = new Date(calculatedDate.getTime());
     calculatedEndDate.setHours(new Date(trainingData.timeTo).getHours());
     calculatedEndDate.setMinutes(new Date(trainingData.timeTo).getMinutes());
     calculatedEndDate.setSeconds(0);
     calculatedEndDate.setMilliseconds(0);
 
+    // Erstelle Training
+    const newTrainingRef = await db.collection("teams")
+        .doc(trainingData.teamId)
+        .collection("trainings")
+        .add({
+          ...trainingData,
+          clubId: teamRef.data().clubId,
+          date: calculatedDate,
+          startDate: calculatedDate,
+          endDate: calculatedEndDate,
+          teamName: teamRef.data().name,
+          liga: teamRef.data().liga,
+        });
 
-    // Add Training Entry
-    const newTrainingRef = await db.collection("teams").doc(trainingData.teamId).collection("trainings").add({
-      ...trainingData,
-      clubId: teamRef.data().clubId,
-      date: calculatedDate,
-      startDate: calculatedDate,
-      endDate: calculatedEndDate,
-      teamName: teamRef.data().name,
-      liga: teamRef.data().liga,
-    });
     console.log("New Training: " + newTrainingRef.id + " " + calculatedDate.toISOString());
     console.log(`Calculated Start Date used: ${calculatedDate}`);
     console.log(`Calculated End Date used: ${calculatedEndDate}`);
+
+    // Berechne nächstes Datum
+    calculatedDate.setTime(calculatedDate.getTime() + offSet);
   } while (calculatedDate.getTime() <= new Date(trainingData.endDate).getTime());
 
-
-  return db.collection("userProfile").doc(userId).collection("trainings").doc(trainingId).delete();
+  return db.collection("userProfile")
+      .doc(userId)
+      .collection("trainings")
+      .doc(trainingId)
+      .delete();
 }
 
 export async function createNotificationTeamTraining(snapshot: QueryDocumentSnapshot, context: functions.EventContext) {
