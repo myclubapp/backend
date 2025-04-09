e20b57aee9a0a0d3e837e3e87e96017f28f7819b
sansan88 <sandro.scalco@liitu.ch>
2025-04-09 14:10:20 +0200

    add content

--- functions/src/firestore/event/addMemberToHelferEvent.ts
+++ functions/src/firestore/event/addMemberToHelferEvent.ts
@@ -16,13 +16,20 @@ export async function addMemberToHelferEvent(event: FirestoreEvent<QueryDocument
   logger.info('clubId: ' + clubId);
 
   const helferEvent = await db.collection('club').doc(clubId).collection('helferEvents').doc(eventId).get();
+  const helferSchicht = await db.collection('club').doc(clubId).collection('helferEvents').doc(eventId).collection('schichten').doc(schichtId).get();
+  const clubData = await db.collection('club').doc(clubId).get();
+
+  // berechne datum anhand eventdatum anhand threshold, welche in stunden angegeben wird
+  const helferEventDatum = new Date(helferEvent.data()?.startDate);
+  const helferEventDatumPlusThreshold = new Date(helferEventDatum.getTime() - clubData.data()?.helferThreshold * 60 * 60 * 1000);
+
 
   const userProfileRef = await db.collection('userProfile').doc(userId).get();
   if (userProfileRef.exists && userProfileRef.data().settingsPush && userProfileRef.data().settingsPushHelfer) {
     await sendPushNotificationByUserProfileId(
         userId,
         'Bestätigung Helferevent: ',
-        helferEvent.data()?.name + ' - ' + helferEvent.data()?.description,
+        helferEvent.data()?.name + ' - ' + helferSchicht.data()?.name,
         {
           'type': 'helferEvent',
           'clubId': clubId,
@@ -36,6 +43,15 @@ export async function addMemberToHelferEvent(event: FirestoreEvent<QueryDocument
         data: {
           helferEventName: helferEvent.data()?.name,
           helferEventDescription: helferEvent.data()?.description,
+          helferEventDatum: helferEvent.data()?.startDate,
+
+          schichtName: helferSchicht.data()?.name,
+          schichtStart: helferSchicht.data()?.timeFrom,
+          schichtEnde: helferSchicht.data()?.timeTo,
+          schichtPunkte: helferSchicht.data()?.points,
+
+          abmeldefrist: helferEventDatumPlusThreshold,
+
           firstName: userProfileRef.data()?.firstName,
           lastName: userProfileRef.data()?.lastName,
         },
