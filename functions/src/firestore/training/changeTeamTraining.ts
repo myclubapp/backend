0098095d185eff8e001ca0495b89f5a731d51328
sansan88 <sandro.scalco@liitu.ch>
2025-05-15 11:48:36 +0200

    feat update

--- functions/src/firestore/training/changeTeamTraining.ts
+++ functions/src/firestore/training/changeTeamTraining.ts
@@ -4,24 +4,11 @@ import firebaseDAO from '../../firebaseSingleton.js';
 import {sendPushNotificationByUserProfileId} from '../../utils/push.js';
 import {FirestoreEvent, Change, QueryDocumentSnapshot} from 'firebase-functions/v2/firestore';
 import {logger} from 'firebase-functions';
-import * as firebase from 'firebase-admin';
 const db = firebaseDAO.instance.db;
 
 export async function changeTeamTrainingCancelled(event: FirestoreEvent<Change<QueryDocumentSnapshot> | undefined>) {
   logger.info('changeTeamTrainingCancelled');
   const {teamId, trainingId} = event.params;
-
-  const trainingRef = await db.collection('teams').doc(teamId).collection('trainings').doc(trainingId).get();
-  const trainingData = trainingRef.data();
-
-  // Attribut aus dem Objekt löschen
-  if (trainingData) {
-    delete trainingData.childId; // oder welches Attribut auch immer Sie löschen möchten
-  }
-
-  await db.collection('teams').doc(teamId).collection('trainings').doc(trainingId).update({
-    childId: firebase.firestore.FieldValue.delete(),
-  });
 
   if (event.data?.after.data()?.cancelled === true &&
     (event.data?.before.data()?.cancelled === false || event.data?.before.data()?.cancelled === undefined)) {
